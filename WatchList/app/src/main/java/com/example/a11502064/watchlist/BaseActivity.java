ae361b9ed0b461267b4d42469b5abbed42c143b7
ArneMotmans <motmans.arne@gmail.com>
2017-10-03 12:30:19 +0200

    Adding basic layout

---
+++ WatchList/app/src/main/java/com/example/a11502064/watchlist/BaseActivity.java
@@ -0,0 +1,73 @@
+package com.example.a11502064.watchlist;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuoptions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menuHome){
+            startActivity(new Intent(this, MainActivity.class));
+
+        } else if (id == R.id.menuWatchlist){
+            startActivity(new Intent(this, Main2Activity.class));
+
+        } else if (id == R.id.menuWatched){
+            startActivity(new Intent(this, Main3Activity.class));
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void basicToastPopular(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "You clicked the 'Popular' tab";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+
+    public void basicToastTopRated(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "You clicked the 'Top Rated' tab";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+
+    public void basicToastUpcoming(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "You clicked the 'Upcoming' tab";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context,text,duration);
+        toast.show();
+    }
+}
---
+++ WatchList/app/src/main/java/com/example/a11502064/watchlist/Main3Activity.java
@@ -0,0 +1,13 @@
+package com.example.a11502064.watchlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
